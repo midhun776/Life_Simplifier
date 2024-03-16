2b7fa7f6c459cf6de79d82bda8ddccd4cb32243c
Vinayak S <vinayaksukhalal@gmail.com>
2024-03-16 21:19:52 +0530

    route configured

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ var bcrypt = require("bcrypt");
 
 // MongoDB Connection to cloud database
 mongoose.connect(
-  "mongodb+srv://vinayaksukhalal:<password>@cluster0.mcgbpy6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-);
+  "mongodb+srv://vinayaksukhalal:12344321@cluster0.mcgbpy6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+).then(()=>{
+    console.log("DB Connected")
+});
 
 //Schema of the User
 var userSchema = mongoose.Schema({
